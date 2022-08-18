839e4f0b3fb1a930b843918a12bb2ab0abde87ac
adrianoseie <aseie@opsguru.io>
2022-08-18 21:29:55 +0200

    feat: adding openvpn tf module

---
+++ main.tf
@@ -0,0 +1,216 @@
+# Uses https://github.com/angristan/openvpn-install to setup VPN in a google VM
+# creates and deletes users accordingly
+
+locals {
+  metadata = merge(var.metadata, {
+    sshKeys = "${var.remote_user}:${tls_private_key.ssh-key.public_key_openssh}"
+  })
+  ssh_tag     = ["allow-ssh"]
+  openvpn_tag = ["openvpn-${var.name}"]
+  tags        = toset(concat(var.tags, local.ssh_tag, local.openvpn_tag))
+
+  output_folder    = var.output_dir
+  private_key_file = "private-key.pem"
+  # adding the null_resource to prevent evaluating this until the openvpn_update_users has executed
+  refetch_user_ovpn = null_resource.openvpn_update_users_script.id != "" ? !alltrue([for x in var.users : fileexists("${var.output_dir}/${x}.ovpn")]) : false
+  name              = var.name == "" ? "" : "${var.name}-"
+  host_project_id   = var.network_project_id != null ? var.network_project_id : var.project_id
+  access_config = [{
+    nat_ip       = google_compute_address.default.address
+    network_tier = var.network_tier
+  }]
+}
+
+resource "google_compute_firewall" "allow-external-ssh" {
+  name    = "openvpn-${var.name}-allow-external-ssh"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = local.ssh_tag
+}
+
+resource "google_compute_firewall" "allow-openvpn-udp-port" {
+  name        = "openvpn-${var.name}-allow"
+  network     = var.network
+  description = "Creates firewall rule targeting the openvpn instance"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1194"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["1194"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = local.openvpn_tag
+}
+
+
+resource "google_compute_address" "default" {
+  name         = "openvpn-${var.name}-global-ip"
+  region       = var.region
+  network_tier = var.network_tier
+}
+
+resource "tls_private_key" "ssh-key" {
+  algorithm = "RSA"
+}
+
+
+// SSH Private Key
+resource "local_sensitive_file" "private_key" {
+  content         = tls_private_key.ssh-key.private_key_pem
+  filename        = "${var.output_dir}/${local.private_key_file}"
+  file_permission = "0400"
+}
+
+resource "random_id" "this" {
+  byte_length = "8"
+}
+
+resource "random_id" "password" {
+  byte_length = "16"
+}
+
+// Use a persistent disk so that it can be remounted on another instance.
+#tfsec:ignore:google-compute-disk-encryption-customer-key
+resource "google_compute_disk" "this" {
+  name  = "openvpn-${var.name}-disk"
+  image = var.image_family
+  size  = var.disk_size_gb
+  type  = var.disk_type
+  zone  = var.zone
+}
+
+#-------------------
+# Instance Template
+#-------------------
+
+module "instance_template" {
+  source  = "terraform-google-modules/vm/google//modules/instance_template"
+  version = "~> 7.3"
+
+  name_prefix        = "openvpn-${var.name}-"
+  project_id         = var.project
+  machine_type       = var.machine_type
+  disk_size_gb       = var.disk_size_gb
+  disk_type          = var.disk_type
+  subnetwork         = var.subnet
+  subnetwork_project = local.host_project_id
+  metadata           = local.metadata
+  service_account = {
+    email  = local.service_account_email
+    scopes = var.scopes
+  }
+  enable_shielded_vm   = var.shielded_vm
+  source_image         = var.image
+  source_image_family  = var.image_family
+  source_image_project = var.image_project
+  preemptible          = var.preemptible
+
+  startup_script = <<SCRIPT
+    curl -O ${var.install_script_url}
+    chmod +x openvpn-install.sh
+    mv openvpn-install.sh /home/${var.remote_user}/
+    chown ${var.remote_user}:${var.remote_user} /home/${var.remote_user}/openvpn-install.sh
+    export AUTO_INSTALL=y
+    export PASS=1
+    # Using Custom DNS
+    export DNS=13
+    export DNS1="${var.dns_servers[0]}"
+    %{if length(var.dns_servers) > 1~}
+    export DNS2="${var.dns_servers[1]}"
+    %{endif~}
+    /home/${var.remote_user}/openvpn-install.sh
+  SCRIPT
+
+  tags   = local.tags
+  labels = var.labels
+
+}
+
+resource "google_compute_instance_from_template" "this" {
+  name    = "openvpn-${var.name}"
+  project = var.project_id
+  zone    = var.zone
+
+  network_interface {
+    subnetwork         = var.subnetwork
+    subnetwork_project = local.host_project_id
+    access_config {
+      nat_ip       = google_compute_address.default.address
+      network_tier = var.network_tier
+    }
+  }
+  source_instance_template = module.instance_template.self_link
+}
+
+# Updates/creates the users VPN credentials on the VPN server
+resource "null_resource" "openvpn_update_users_script" {
+  triggers = {
+    users    = join(",", var.users)
+    instance = google_compute_instance_from_template.this.instance_id
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.remote_user
+    host        = google_compute_address.default.address
+    private_key = tls_private_key.ssh-key.private_key_pem
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/scripts/update_users.sh"
+    destination = "/home/${var.remote_user}/update_users.sh"
+    when        = create
+  }
+
+  # Create New User with MENU_OPTION=1
+  provisioner "remote-exec" {
+    inline = [
+      "while [ ! -f /etc/openvpn/server.conf ]; do sleep 10; done",
+      "chmod +x ~${var.remote_user}/update_users.sh",
+      "sudo ROUTE_ONLY_PRIVATE_IPS='${var.route_only_private_ips}' ~${var.remote_user}/update_users.sh ${join(" ", var.users)}",
+    ]
+    when = create
+  }
+
+  # Delete OVPN files if new instance is created
+  provisioner "local-exec" {
+    command = "rm -rf ${abspath(var.output_dir)}/*.ovpn"
+    when    = create
+  }
+
+  depends_on = [google_compute_instance_from_template.this, local_sensitive_file.private_key]
+}
+
+
+# Download user configurations to output_dir
+resource "null_resource" "openvpn_download_configurations" {
+  triggers = {
+    trigger = timestamp()
+  }
+
+  depends_on = [null_resource.openvpn_update_users_script]
+
+  # Copy .ovpn config for user from server to var.output_dir
+  provisioner "local-exec" {
+    working_dir = var.output_dir
+    command     = "${abspath(path.module)}/scripts/refetch_user.sh"
+    environment = {
+      REFETCH_USER_OVPN = local.refetch_user_ovpn
+      PRIVATE_KEY_FILE  = local.private_key_file
+      REMOTE_USER       = var.remote_user
+      IP_ADDRESS        = google_compute_address.default.address
+    }
+    when = create
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,127 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  default     = null
+}
+
+variable "network_project_id" {
+  description = "The GCP Host Project ID"
+  default     = null
+}
+
+variable "name" {
+  type        = string
+  description = "The name to use when generating resources"
+}
+
+variable "region" {
+  description = "The GCP Project Region"
+  default     = null
+}
+
+variable "zone" {
+  description = "The GCP Zone to deploy VPN Compute instance to"
+}
+
+variable "network" {
+  description = "The name or self_link of the network to attach this interface to. Use network attribute for Legacy or Auto subnetted networks and subnetwork for custom subnetted networks."
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "The name of the subnetwork to attach this interface to. The subnetwork must exist in the same region this instance will be created in. Either network or subnetwork must be provided."
+  default     = null
+}
+
+variable "image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "disk_type" {
+  description = "(Optional) The GCE disk type. Can be either pd-ssd, local-ssd, pd-balanced or pd-standard"
+  default     = "pd-standard"
+}
+
+variable "disk_size_gb" {
+  type    = string
+  default = "30"
+}
+
+variable "auto_delete_disk" {
+  description = "Whether or not the boot disk should be auto-deleted"
+  default     = false
+}
+
+variable "service_account" {
+  default = {
+    email  = null
+    scopes = []
+  }
+  type = object({
+    email  = string,
+    scopes = set(string)
+  })
+  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#service_account."
+}
+
+variable "metadata" {
+  description = "Metadata, provided as a map"
+  default     = {}
+}
+
+variable "network_tier" {
+  description = "Network network_tier"
+  default     = "PREMIUM"
+}
+
+variable "labels" {
+  default     = {}
+  description = "Labels, provided as a map"
+}
+
+variable "users" {
+  default     = []
+  type        = list(string)
+  description = "list of user to create"
+}
+
+variable "tags" {
+  description = "network tags to attach to the instance"
+  default     = []
+}
+
+variable "output_dir" {
+  description = "Folder to store all user openvpn details"
+  default     = "openvpn"
+}
+
+variable "remote_user" {
+  description = "The user to operate as on the VM. SSH Key is generated for this user"
+  default     = "ubuntu"
+}
+
+variable "machine_type" {
+  description = "Machine type to create, e.g. n1-standard-1"
+  default     = "n1-standard-1"
+}
+
+variable "route_only_private_ips" {
+  description = "Routes only private IPs through the VPN (10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16)"
+  default     = false
+}
+
+variable "install_script_url" {
+  description = "The commit sha we are using in order to determine which version of the install file to use: https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh"
+  type        = string
+  default     = "https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh"
+}
+
+variable "dns_servers" {
+  description = "The DNS servers to be configured"
+  default     = ["8.8.8.8", "8.8.4.4"]
+  type        = list(string)
+  validation {
+    condition     = length(var.dns_servers) >= 1 || length(var.dns_servers) <= 2
+    error_message = "The variable 'var.dns_servers' should be an array with 1 or 2 DNS entries only."
+  }
+}
