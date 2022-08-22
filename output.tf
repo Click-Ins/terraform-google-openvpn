c3a97d1b91469192bfc273c7ce7e7a8573122653
adrianoseie <aseie@opsguru.io>
2022-08-22 12:42:38 +0200

    added NAT IP output

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "users" {
 
 output "public_ip" {
   description = "Public IP for of the openvpn server"
-  value       = google_compute_address.default.address
+  value       = google_compute_instance_from_template.this.network_interface[0].access_config[0].nat_ip
 }
